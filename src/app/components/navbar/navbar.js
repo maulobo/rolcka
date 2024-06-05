fc01c184d811cb272a7a18633b9dcbfcbd808743
Mauro <maurolobo.ml@gmail.com>
2024-06-05 11:52:37 -0300

    agrego fabricacion

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -6,7 +6,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 const navigation = [
   { name: "Home", href: "/", current: true },
   { name: "Servicios", href: "/servicios", current: false },
-  { name: "Proyectos", href: "#", current: false },
+  { name: "Fabricacion", href: "/fabricacion", current: false },
 ];
 
 function classNames(...classes) {
